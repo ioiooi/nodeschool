ff8ffcca06ca3589e3ccdded4cc3b65a46bf85a8
Eugen Mayer <eugen_mayer@gmx.net>
2015-08-31 20:06:34 +0200

    planetproto

--- PlanetProto/020.js
+++ PlanetProto/020.js
@@ -1,31 +1,35 @@
 // -> Create a machine object
 //    with a property motors = 4
-__
+var machine = {
+	motors: 4
+}
 
 // -> Create a robot object
 //    with a property friendly = true
-__
+var robot = {
+	friendly: true
+}
 
 // -> Create a robby object
-__
+var robby = {}
 
 // -> Make machine the prototype of robot
-__
+robot.__proto__ = machine;
 
 // -> Make robot the prototype of robby
-__
+robby.__proto__ = robot;
 
 // -> What is `robby.motors`?
-claim(robby.motors, __);
+claim(robby.motors, 4);
 
 // -> What is `robby.friendly`?
-claim(robby.friendly, __);
+claim(robby.friendly, true);
 
 
 // ------------------------------------------------
 // Common JS exports for verification, don't modify
 module.exports = {
 	machine: machine,
-	robot:   robot,
-	robby:   robby
+	robot: robot,
+	robby: robby
 }
